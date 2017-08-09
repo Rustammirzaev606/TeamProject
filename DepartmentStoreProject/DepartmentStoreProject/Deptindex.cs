2a8b02eeb989c5f9199340ab3603e9f30be0f1d3
Ravend20 <Fleetsd@yahoo.com>
2017-08-09 14:07:40 -0400

    55

---
+++ DepartmentStoreProject/DepartmentStoreProject/Deptindex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentStoreProject
+{
+    class Deptindex
+    {
+        Program p = new Program();
+        public string Store;
+        public int ClothingID;
+        public string Section;
+        public int Price;
+
+        public void FullStoreName()
+        {
+
+            Console.WriteLine($"Hello and Welcome to {Store}");
+
+        }
+
+        public void 
+             
+
+
+
+
+
+    }
+}
