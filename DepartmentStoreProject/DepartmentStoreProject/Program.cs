e0d3006ec2e4ff8749b47d4412118aca56b1655a
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-10 11:08:45 -0400

    asd

--- DepartmentStoreProject/DepartmentStoreProject/Program.cs
+++ DepartmentStoreProject/DepartmentStoreProject/Program.cs
@@ -19,13 +19,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DepartmentStoreProject
 {
-    class Program
+    enum Genres
     {
+        Shoes = 1,
+        Pants,
+        Shirts,
+        Hats
+
+    }
+    class Program
+    {        
         static void Main(string[] args)
         {
-            //HI THERE
+            
+            string inputChoice;
+            int inputNumb;
+            
+            
+            string exit = "exit";
+            Console.WriteLine("Welcome to our store!");
+            do
+            {
+                
+                Console.WriteLine("Select the department to shop:\n1 - Shoes:\n2 - Pants:\n3 - Shirts:\n4 - Hats:");
+                inputChoice = Console.ReadLine();
+                int.TryParse(inputChoice, out inputNumb);
+                while (!int.TryParse(inputChoice, out inputNumb))
+                {
+
+                    Console.WriteLine("INCORRECT INPUT! SELECT A NUMBER OF A DEPARTMENT:\n1 - Shoes:\n2 - Pants:\n3 - Shirts:\n4 - Hats:");
+                }
+                switch ((Genres)inputNumb)
+                {
+                    case Genres.Shoes:
+                        //call method1
+                        Console.WriteLine("123");
+                        break;
+                    case Genres.Pants:
+                        Console.WriteLine("123");
+                        break;
+                    case Genres.Shirts:
+                        Console.WriteLine("123");
+                        break;
+                    case Genres.Hats:
+                        Console.WriteLine("123");
+                        break;
+                }
+
+            } while (!(exit == "exit"));
+            Console.WriteLine("Your total is");
+            //method1
+            //ask what item they want. Get input. Case choice. Add mathing price to subtotal. 
+
+        
+            
         }
     }
 }
